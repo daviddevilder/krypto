d014886198c936074a30ce6ca599322e7c38b1d9
daviddevilder <davidd@fantoo.net>
2018-05-30 17:21:04 +0100

    Unit tests for UI

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,19 +3,25 @@ import {AppComponent} from './app.component';
 import {TokenListComponent} from "./token-list/token-list.component";
 import {TokenService} from "./services/token.service";
 import {HttpClient, HttpHandler} from "@angular/common/http";
+import {PortfolioListComponent} from "./portfolio-list/portfolio-list.component";
+import {PortfolioService} from "./services/portfolio.service";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            providers: [TokenService, HttpClient, HttpHandler],
-            declarations: [AppComponent, TokenListComponent]
+            providers: [TokenService, PortfolioService, HttpClient, HttpHandler],
+            declarations: [AppComponent, TokenListComponent, PortfolioListComponent],
+            imports: [FormsModule, ReactiveFormsModule]
         }).compileComponents();
     }));
+
     it('should create the app', async(() => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
     }));
+
     it(`should have the correct page title`, async(() => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
--- src/app/portfolio-list/portfolio-list.component.ts
+++ src/app/portfolio-list/portfolio-list.component.ts
@@ -4,6 +4,7 @@ import {Portfolio} from "../../../common/models/Portfolio";
 import {FormGroup, FormBuilder, FormArray} from "@angular/forms";
 import {Token} from "../../../common/models/Token";
 import {TokenService} from "../services/token.service";
+import {Holding} from "../../../common/models/Holding";
 
 @Component({
     selector: 'app-portfolio-list',
@@ -22,36 +23,51 @@ export class PortfolioListComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.portfolioForm = this.formBuilder.group({
-            name: '',
-            holdings: this.formBuilder.array([this.createHolding()])
-        });
+        this.resetForm();
 
         this.tokenService.LoadTokens()
             .subscribe((tokens: Token[]) => {
                 this.Tokens = tokens;
-                this.portfolioService.LoadPortfolios()
-                    .subscribe((portfolios: Portfolio[]) => {
-                        this.Portfolios = portfolios;
-                        this.Portfolios.forEach((portfolio) => {
-                            portfolio.totalValue = 0;
-                            portfolio.holdings.forEach(holding => {
-                                var token: Token = this.Tokens.filter(function (token) {
-                                    return token.id == holding.tokenId;
-                                })[0];
-                                holding.tokenName = token.name;
-                                holding.value = Number(token.price) * Number(holding.quantity);
-                                portfolio.totalValue += holding.value;
-                            });
-                        });
-                    });
+                this.loadPortfolios();
             });
     }
+
+    private resetForm = function () {
+        this.portfolioForm = this.formBuilder.group({
+            name: '',
+            holdings: this.formBuilder.array([this.createHolding()])
+        });
+    };
+
+    private loadPortfolios = function () {
+        this.portfolioService.LoadPortfolios()
+            .subscribe((portfolios: Portfolio[]) => {
+                this.Portfolios = portfolios;
+                this.calculatePortfolioValues();
+            });
+    };
+
+    private calculatePortfolioValues = function () {
+        this.Portfolios.forEach((portfolio: Portfolio) => {
+            portfolio.totalValue = 0;
+            portfolio.holdings.forEach(holding => this.deriveNameAndValue(portfolio, holding));
+        });
+    };
+
+    private deriveNameAndValue = function (portfolio: Portfolio, holding: Holding) {
+        var token: Token = this.Tokens.filter(function (token) {
+            return token.id == holding.tokenId;
+        })[0];
+
+        holding.tokenName = token.name;
+        holding.value = Number(token.price) * Number(holding.quantity);
+        portfolio.totalValue += holding.value;
+    };
 
     createHolding(): FormGroup {
         return this.formBuilder.group({
             tokenId: '',
-            quantity: ''
+            quantity: 0
         });
     }
 
@@ -64,10 +80,23 @@ export class PortfolioListComponent implements OnInit {
     }
 
     createPortfolio(formValue: any) {
+        formValue.holdings = this.removeEmpties(formValue.holdings);
         this.portfolioService.CreatePortfolio(formValue)
             .subscribe(() => {
                 alert("Created successfully!!!");
+                this.resetForm();
+                this.loadPortfolios();
             });
+    }
+
+    private removeEmpties = function (holdings: Holding[]) {
+        const results = [];
+        holdings.forEach((holding) => {
+            if (holding.tokenId.length > 0 && holding.quantity > 0) {
+                results.push(holding)
+            }
+        });
+        return results;
     }
 
 }
